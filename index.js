cdda2ebb15fb97162a1c01b6a7ba66f11ebeea5f
Katsumi Takahashi <katsumi@gij.com>
2022-08-12 08:25:33 +0900

    ループ方法を変更

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * Remove recordings with date range.
- *  
+ *
  */
 'use strict';
 
@@ -10,32 +10,37 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const moment = require('moment');
 
-const START_DATE = moment(process.env.START_DATE);      // Range start
-const END_DATE = moment(process.env.END_DATE);          // Range end
+const START_DATE = moment(process.env.START_DATE); // Range start
+const END_DATE = moment(process.env.END_DATE); // Range end
 
-const delLoop = (targetDate => {
-    let _targetDate = targetDate || START_DATE;
+const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec)); // タイマー
 
-    // Extract data fro one day.
-    client.recordings.list({
-        dateCreated: _targetDate.toDate()
-    })
-    .then(recordings => {
-        // Remove the retrieved list.
-        recordings.forEach(async r => {
-            await client.recordings(r.sid).remove();
-            console.log(`${r.sid} ${r.dateCreated} deleted.`);
-        });
+const delLoop = (targetDate) => {
+  let _targetDate = targetDate || START_DATE;
 
-        // If the end date has not been reached, call it recursively.
-        if (_targetDate.isBefore(END_DATE)) {
-            _targetDate = _targetDate.add(1, 'days');
-            delLoop(_targetDate);
-        }
+  // Extract data fro one day.
+  client.recordings
+    .list({
+      dateCreated: _targetDate.toDate(),
     })
-    .catch(err => {
-        console.log(err);
+    .then(async (recordings) => {
+      for (let r of recordings) {
+        // if (r.channels === 1) {
+        await client.recordings(r.sid).remove();
+        console.log(`${r.sid} ${r.dateCreated} deleted.`);
+        await sleep(20);
+        // }
+      }
+
+      // If the end date has not been reached, call it recursively.
+      if (_targetDate.isBefore(END_DATE)) {
+        _targetDate = _targetDate.add(1, 'days');
+        delLoop(_targetDate);
+      }
     })
-});
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 delLoop();
